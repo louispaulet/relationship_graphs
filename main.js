d48b00fe6510222a62b32f2efbce9af5fa3b775e
louispaulet <louispaulet13@gmail.com>
2023-05-15 04:52:23 +0200

    refactor load_data() function into smaller functions

--- main.js
+++ main.js
@@ -30,9 +30,15 @@ function loadData() {
   // Get the current value of the slider
   const linkDistance = document.getElementById('link-distance').value;
 
-  d3.json("json_files/"+filename+".json").then(data => {
+  d3.json("json_files/" + filename + ".json").then(data => {
     // Clear the previous graph
     d3.select("#graph").html("");
+    
+    createGraph(data, linkDistance);
+  });
+}
+
+function createGraph(data, linkDistance) {
   const width = 800;
   const height = 600;
 
@@ -41,88 +47,110 @@ function loadData() {
     .attr("width", width)
     .attr("height", height);
 
-  const link = svg.append("g")
-    .attr("class", "links")
-    .selectAll("line")
-    .data(data.links)
-    .join("line")
-    .attr("class", "link");
-    
-  const linkLabel = svg.append("g")
-  .attr("class", "link-labels")
-  .selectAll("text")
-  .data(data.links)
-  .join("text")
-  .attr("class", "link-label")
-  .text(d => d.label);
+  const link = createLink(svg, data);
+  const linkLabel = createLinkLabel(svg, data);
+  const node = createNode(svg, data);
+  const label = createLabel(svg, data);
 
-  const node = svg.append("g")
-    .attr("class", "nodes")
-    .selectAll("circle")
-    .data(data.nodes)
-    .join("circle")
-    .attr("class", "node")
-    .attr("r", 20);
-
-  const label = svg.append("g")
-    .attr("class", "labels")
-    .selectAll("text")
-    .data(data.nodes)
-    .join("text")
-    .attr("class", "label")
-    .text(d => d.label);
-
-  const simulation = d3.forceSimulation(data.nodes)
-    .force("link", d3.forceLink(data.links).id(d => d.id).distance(linkDistance))
-    .force("charge", d3.forceManyBody())
-    .force("collide", d3.forceCollide(50))
-    .force("center", d3.forceCenter(width / 2, height / 2));
-
+  const simulation = createSimulation(data, linkDistance, width, height);
 
   simulation.on("tick", () => {
-    link
-      .attr("x1", d => d.source.x)
-      .attr("y1", d => d.source.y)
-      .attr("x2", d => d.target.x)
-      .attr("y2", d => d.target.y);
-
-    node
-      .attr("cx", d => d.x)
-      .attr("cy", d => d.y);
-
-    label
-      .attr("x", d => d.x)
-      .attr("y", d => d.y);
-      
-    linkLabel
-    .attr("x", d => (d.source.x + d.target.x) / 2)
-    .attr("y", d => (d.source.y + d.target.y) / 2);
+    updateGraph(link, node, label, linkLabel);
   });
 
   node.call(d3.drag()
     .on("start", dragstart)
     .on("drag", dragged)
     .on("end", dragend));
-
-  function dragstart(event) {
-    if (!event.active) simulation.alphaTarget(0.3).restart();
-    event.subject.fx = event.subject.x;
-    event.subject.fy = event.subject.y;
-  }
-
-  function dragged(event) {
-    event.subject.fx = event.x;
-    event.subject.fy = event.y;
-  }
-
-  function dragend(event) {
-    if (!event.active) simulation.alphaTarget(0);
-    event.subject.fx = null;
-    event.subject.fy = null;
-  }
-});
- 
 }
+
+function updateGraph(link, node, label, linkLabel) {
+  link
+    .attr("x1", d => d.source.x)
+    .attr("y1", d => d.source.y)
+    .attr("x2", d => d.target.x)
+    .attr("y2", d => d.target.y);
+
+  node
+    .attr("cx", d => d.x)
+    .attr("cy", d => d.y);
+
+  label
+    .attr("x", d => d.x)
+    .attr("y", d => d.y);
+      
+  linkLabel
+    .attr("x", d => (d.source.x + d.target.x) / 2)
+    .attr("y", d => (d.source.y + d.target.y) / 2);
+}
+
+// helper functions for creating the elements of the graph
+function createLink(svg, data) {
+  return svg.append("g")
+    .attr("class", "links")
+    .selectAll("line")
+    .data(data.links)
+    .join("line")
+    .attr("class", "link");
+}
+
+function createLinkLabel(svg, data) {
+  return svg.append("g")
+  .attr("class", "link-labels")
+  .selectAll("text")
+  .data(data.links)
+  .join("text")
+  .attr("class", "link-label")
+  .text(d => d.label);
+}
+
+function createNode(svg, data) {
+  return svg.append("g")
+    .attr("class", "nodes")
+    .selectAll("circle")
+    .data(data.nodes)
+    .join("circle")
+    .attr("class", "node")
+    .attr("r", 20);
+}
+
+function createLabel(svg, data) {
+  return svg.append("g")
+    .attr("class", "labels")
+    .selectAll("text")
+    .data(data.nodes)
+    .join("text")
+    .attr("class", "label")
+    .text(d => d.label);
+}
+
+function createSimulation(data, linkDistance, width, height) {
+  return d3.forceSimulation(data.nodes)
+    .force("link", d3.forceLink(data.links).id(d => d.id).distance(linkDistance))
+    .force("charge", d3.forceManyBody())
+    .force("collide", d3.forceCollide(50)).force("center", d3.forceCenter(width / 2, height / 2));
+}
+
+// drag-related functions
+function dragstart(event) {
+  if (!event.active) this.alphaTarget(0.3).restart();
+  event.subject.fx = event.subject.x;
+  event.subject.fy = event.subject.y;
+}
+
+function dragged(event) {
+  event.subject.fx = event.x;
+  event.subject.fy = event.y;
+}
+
+function dragend(event) {
+  if (!event.active) this.alphaTarget(0);
+  event.subject.fx = null;
+  event.subject.fy = null;
+}
+
+   
+
 
 function createNewSVGWithBackground(svgElement) {
   const svgWithBackground = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
